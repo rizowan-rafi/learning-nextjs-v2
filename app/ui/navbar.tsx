16d12175ad6e6c1806ba2aa07989c26b71537d9b
isagi ego <isagiego299@gmail.com>
2026-06-29 13:09:08 +0600

    complete chapter 13. start auth

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <nav className="p-4 bg-gray-100 text-white dark:bg-gray-900 border-b">
             <div className="container mx-auto flex justify-between items-center">
                 <span className="font-bold">My Logo</span>
                 <div className="space-x-4">
-                    <a href="/" className="hover:underline">
+                    <Link href="/" className="hover:underline">
                         Home
-                    </a>
-                    <a href="/about" className="hover:underline">
+                    </Link>
+                    <Link href="/about" className="hover:underline">
                         About
-                    </a>
+                    </Link>
                 </div>
             </div>
         </nav>
